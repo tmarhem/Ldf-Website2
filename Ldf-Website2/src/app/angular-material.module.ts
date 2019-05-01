6d9512b5221c0cebf34ec0c60d5193db0ee0d6d0
Thibault Marhem <t.marhem@gmail.com>
2019-05-01 21:57:10 +0200

    some starting points

---
+++ Ldf-Website2/src/app/angular-material.module.ts
@@ -0,0 +1,84 @@
+import { NgModule } from '@angular/core';
+import {
+  MatTableModule, MatIconModule, MatButtonModule, MatToolbarModule, MatCardModule, MatMenuModule, MatInputModule,
+  MatPaginatorModule, MatSortModule, MatTabsModule, MatDatepickerModule, MatDialogModule, MatAutocompleteModule,
+  MatSelectModule, MatFormFieldModule, MatCheckboxModule, MatListModule, MatSnackBarModule,
+} from '@angular/material';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatNativeDateModule} from '@angular/material';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  imports: [
+    MatFormFieldModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatListModule,
+    MatRadioModule,
+    MatNativeDateModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatSnackBarModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatListModule,
+    MatRadioModule,
+    MatNativeDateModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatNativeDateModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatSnackBarModule
+  ]
+})
+export class AngularMaterialModule { }
