b448aeb59cc37c4d5bcfa75a37a42218ef87ea5e
Thibault Marhem <thibault.marhem@atos.net>
2023-08-05 10:02:42 +0200

    feat: lang fr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit, Inject } from '@angular/core';
 import {Location} from '@angular/common';
 import { MatSidenav} from '@angular/material';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { ContactComponent } from './components/association/contact/contact.component';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -27,9 +28,12 @@ export class AppComponent implements OnInit{
   constructor(
     public location: Location,
     public dialog: MatDialog,
-    private _router: Router) { }
+    private _router: Router,
+    @Inject(DOCUMENT) private document: Document) { }
 
     ngOnInit(): void {
+      this.document.documentElement.lang = 'fr'; 
+
       if ( this.location.path() === `/home` || this.location.path() === ``) {
         this.sidenav.close();
       } else {
